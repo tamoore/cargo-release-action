49a52f1178355b2bd7763391b2fde8b4c76c6116
Automated Publisher <actions@users.noreply.github.com>
2020-03-09 21:54:07 +1100

    feat: Add releasing capability with GITHUB_TOKEN

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,14 +1,42 @@
 #!/usr/bin/env bash
-set -e
+set -eu
+declare REMOTE="${REMOTE:-cargo}"
+declare GITHUB_TOKEN="${GITHUB_TOKEN:-}"
+declare GITHUB_ACTOR="${GITHUB_ACTOR:-}"
+declare GITHUB_REPOSITORY="${GITHUB_REPOSITORY:-}"
+declare INPUT_DRY_RUN="${INPUT_DRY_RUN:-false}"
+declare INPUT_VERSION="${INPUT_VERSION:-}"
 
 main() {
+   # check to see if we have a GITHUB_TOKEN set other exit
+   is_a_token_set
+
+   # initialize git settings for pushing tags and updated manifests
+   setup_git
+
    if [[ "${INPUT_DRY_RUN}" == "true" ]]; then
-      cargo release "${INPUT_VERSION}" --dry-run --no-dev-version
+      cargo release "${INPUT_VERSION}" --dry-run --no-dev-version --push-remote "${REMOTE}"
    fi
 
    if [[ "${INPUT_DRY_RUN}" == "false" ]]; then
-      cargo release "${INPUT_VERSION}" --no-dev-version
+      cargo release "${INPUT_VERSION}" --no-dev-version --push-remote "${REMOTE}"
    fi
+}
+
+is_a_token_set() {
+   if [[ -z "${GITHUB_TOKEN:-}" ]]; then
+    echo "No GITHUB_TOKEN set"
+    exit 1
+   fi
+}
+
+setup_git() {
+   local remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+   git config http.sslVerify false
+   git config user.name "Automated Publisher"
+   git config user.email "actions@users.noreply.github.com"
+   git remote add "${REMOTE}" "${remote_repo}"
+   git show-ref # useful for debugging
 }
 
 main
